8ce94346ac8526fd93b476b580bdfaeb799b5f92
rezaaminimajd <reza.amini.majd1379@gmail.com>
2020-05-29 15:17:20 +0430

    create serializers class

--- apps/store/serializers.py
+++ apps/store/serializers.py
@@ -1,10 +1,39 @@
 from rest_framework import serializers
-from .models import Clothe
+from .models import *
+
+
+class ClotheSizeSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = ClotheSize
+        exclude = ['id']
+
+
+class ClotheColorSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = ClotheColor
+        exclude = ['id']
+
+
+class ClotheKindSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = ClotheKind
+        exclude = ['id']
+
+
+class ClotheInfoSerializers(serializers.ModelSerializer):
+    color = ClotheColorSerializers
+    size = ClotheSizeSerializers
+
+    class Meta:
+        model = ClotheSize
+        fields = ['count', 'color', 'size']
 
 
 class ClotheSerializers(serializers.ModelSerializer):
+    kind = ClotheKindSerializers
+    clothe_info = ClotheInfo
+
     class Meta:
         model = Clothe
-        fields = ['__all__']
-
-
+        fields = ['code', 'price', 'discounted_price', 'is_discounted',
+                  'category', 'kind', 'description', 'clothe_info']
