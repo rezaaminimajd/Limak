46cb03d2e274607deb3b4b3b32126ea346356b3a
alireza <alirezashateri7@gmail.com>
2020-06-12 17:39:38 +0430

    Add seeder

--- apps/accounts/seed.py
+++ apps/accounts/seed.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+
 from .models import User, Profile, Address
 
 
@@ -5,7 +7,9 @@ def seed_accounts_app(users=5, addresses=10):
     from django_seed import Seed
     seeder = Seed.seeder()
     seeder.add_entity(User, users)
-    seeder.add_entity(Profile, users)
+    # seeder.add_entity(Profile, users)
     seeder.add_entity(Address, addresses)
-
-
+    pks = seeder.execute()
+    for pk in pks[User]:
+        Profile.objects.create(user_id=pk, birth_date=timezone.now(),
+                               phone_number='0123456789')
