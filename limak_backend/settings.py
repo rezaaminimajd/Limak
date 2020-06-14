740b0f48841f84fe70032be2321130f398684b5d
alireza <alirezashateri7@gmail.com>
2020-06-15 01:22:20 +0430

    Add rest authentication to settings

--- limak_backend/settings.py
+++ limak_backend/settings.py
@@ -125,6 +125,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_AUTHENTICATION_PERMISSIONS': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 4
 }
