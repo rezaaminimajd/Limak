86210a3b4ec21e5aa64eefe0d97eceebbfa88735
alireza <alirezashateri7@gmail.com>
2020-06-17 00:40:14 +0430

    Modify celery config

--- limak_backend/celery.py
+++ limak_backend/celery.py
@@ -6,9 +6,10 @@ from django.conf import settings
 from celery import Celery
 from celery.signals import setup_logging
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gamein_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'limak_backend.settings.production')
 
-app = Celery('gamein_backend', broker='amqp://localhost:5672')
+app = Celery('limak_backend', broker='amqp://localhost:5672')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 
