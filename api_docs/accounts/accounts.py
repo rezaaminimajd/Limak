206a2d107a11c3a1ef117cea8cd284d34931f487
alireza <alirezashateri7@gmail.com>
2020-06-19 21:16:00 +0430

    Add accounts doc

---
+++ api_docs/accounts/accounts.py
@@ -0,0 +1,200 @@
+""" API 1.
+
+Name: 
+
+URL: 
+
+Method: 
+
+Description: 
+
+
+  Request json example: 
+"""
+
+Json_1 = {
+
+    # Leave Emtpy if not needed
+
+}
+
+""" API 2.
+
+Name: 
+
+URL: 
+
+Method: 
+
+Description: 
+
+
+  Request json example: 
+"""
+
+Json_2 = {
+
+    # Leave Emtpy if not needed
+
+}
+
+""" API 3.
+
+Name: 
+
+URL: 
+
+Method: 
+
+Description: 
+
+
+  Request json example: 
+"""
+
+Json_3 = {
+
+    # Leave Emtpy if not needed
+
+}
+
+""" API 4.
+
+Name: 
+
+URL: 
+
+Method: 
+
+Description: 
+
+
+  Request json example: 
+"""
+
+Json_4 = {
+
+    # Leave Emtpy if not needed
+
+}
+
+""" API 5.
+
+Name: 
+
+URL: 
+
+Method: 
+
+Description: 
+
+
+  Request json example: 
+"""
+
+Json_5 = {
+
+    # Leave Emtpy if not needed
+
+}
+
+""" API 6.
+
+Name: 
+
+URL: 
+
+Method: 
+
+Description: 
+
+
+  Request json example: 
+"""
+
+Json_6 = {
+
+    # Leave Emtpy if not needed
+
+}
+
+""" API 7.
+
+Name: 
+
+URL: 
+
+Method: 
+
+Description: 
+
+
+  Request json example: 
+"""
+
+Json_7 = {
+
+    # Leave Emtpy if not needed
+
+}
+
+""" API 8.
+
+Name: 
+
+URL: 
+
+Method: 
+
+Description: 
+
+
+  Request json example: 
+"""
+
+Json_8 = {
+
+    # Leave Emtpy if not needed
+
+}
+
+""" API 9.
+
+Name: 
+
+URL: 
+
+Method: 
+
+Description: 
+
+
+  Request json example: 
+"""
+
+Json_9 = {
+
+    # Leave Emtpy if not needed
+
+}
+
+""" API 10.
+
+Name: 
+
+URL: 
+
+Method: 
+
+Description: 
+
+
+  Request json example: 
+"""
+
+Json_10 = {
+
+    # Leave Emtpy if not needed
+
+}
+
