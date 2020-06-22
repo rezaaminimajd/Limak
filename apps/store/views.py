c86b4c32d3507fd055a96e1cffc17454dfb0c282
alireza <alirezashateri7@gmail.com>
2020-06-23 00:00:25 +0430

    Update page size in StorePagination

--- apps/store/views.py
+++ apps/store/views.py
@@ -14,7 +14,7 @@ from .services.filter import Filter
 
 
 class StorePagination(PageNumberPagination):
-    page_size = 10
+    page_size = 12
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
