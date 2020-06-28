19c061b5adc31b17abea88fa66ea035d17af009f
alireza <alirezashateri7@gmail.com>
2020-06-28 23:42:52 +0430

    Fix filter service after changes in ClotheInfo model

--- apps/store/services/filter.py
+++ apps/store/services/filter.py
@@ -12,9 +12,9 @@ class Filter:
         for obj in self.objects:
             if (obj.category.name in category or len(category) == 0) and (
                     obj.kind.name in kind or len(kind) == 0):
-                for info in obj.information.all():
-                    if (info.color.name in color or len(color) == 0) and (
-                            info.size.name in size or len(size) == 0):
-                        filtered_objects.append(obj)
+                info = obj.information
+                if (info.color.name in color or len(color) == 0) and (
+                        info.size.name in size or len(size) == 0):
+                    filtered_objects.append(obj)
 
         return filtered_objects
